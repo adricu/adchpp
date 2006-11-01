f27178741c82fa20f909f007b2dacebfe6e5da25
arnetheduck <devnull@localhost>
2006-11-01 15:03:45 +0000

    better fix

--- adchpp/ManagedSocket.cpp
+++ adchpp/ManagedSocket.cpp
@@ -133,7 +133,7 @@ bool ManagedSocket::completeWrite(ByteVector* buf, size_t written) throw() {
 			}
 		} 
 		moreData = (outBuf != 0) || disc > 0;
-		if( !moreData || (disc > 0) || (outBuf->size() < (size_t)SETTING(MAX_BUFFER_SIZE)) )
+		if( !outbuf || (outBuf->size() < (size_t)SETTING(MAX_BUFFER_SIZE)) )
 			overFlow = 0;
 			
 	}
