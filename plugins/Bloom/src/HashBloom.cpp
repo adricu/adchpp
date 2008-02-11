96723babddc910c1878d07c2d0a3a46c7a5b2edd
arnetheduck <devnull@localhost>
2008-02-11 19:20:52 +0000

    cleanup  bloom pos

--- plugins/Bloom/src/HashBloom.cpp
+++ plugins/Bloom/src/HashBloom.cpp
@@ -53,12 +53,13 @@ void HashBloom::reset(ByteVector& v, size_t k_, size_t h_) {
 }
 
 size_t HashBloom::pos(const TTHValue& tth, size_t n) const {
-	uint64_t x = 0;
-	
-	size_t start = n * h;
 	if((n+1)*h > TTHValue::BITS) {
 		return 0;
 	}
+
+	uint64_t x = 0;
+	
+	size_t start = n * h;
 	
 	for(size_t i = 0; i < h; ++i) {
 		size_t bit = start + i;
