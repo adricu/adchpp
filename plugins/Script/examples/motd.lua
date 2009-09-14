1c4a19cf5d1bbe5e2643453d76b87f29572f12c6
poy <poy@123gen.com>
2009-09-14 23:18:11 +0200

    MOTD

---
+++ plugins/Script/examples/motd.lua
@@ -0,0 +1,34 @@
+-- Script to display a Message Of The Day when users connect.
+
+local base = _G
+
+module("motd")
+base.require('luadchpp')
+local adchpp = base.luadchpp
+
+-- Where to read MOTD file
+local file = adchpp.Util_getCfgPath() .. "motd.txt"
+
+local io = base.require('io')
+local string = base.require('string')
+local autil = base.require('autil')
+
+local motd
+
+local function load_motd()
+	local fp = io.open(file, "r")
+	if not fp then
+		base.print("Unable to open " .. file ..", MOTD not loaded")
+		return
+	end
+	motd = fp:read("*a")
+	fp:close()
+end
+
+base.pcall(load_motd)
+
+motd_1 = adchpp.getCM():signalState():connect(function(entity)
+	if entity:getState() == adchpp.Entity_STATE_NORMAL then
+		autil.reply(entity, motd)
+	end
+end)
