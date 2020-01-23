6b7b40b896385693f00670a481c62c89f4acecbb
eMTee <emtee11@gmail.com>
2020-01-23 16:47:03 +0100

    Fix certificate generation on Windows

--- windows/Generate_certs.cmd
+++ windows/Generate_certs.cmd
@@ -1,17 +1,20 @@
 @echo off
 echo ADCH++ Certifcation Generator
 echo.
-echo This script reqires Win32 OpenSSL Installed
-echo Win32 OpenSSL - http://www.slproweb.com/products/Win32OpenSSL.html
+echo This script requires the newest Win32-OpenSSL binaries to be installed.
+echo Available from http://www.slproweb.com/products/Win32OpenSSL.html
 echo.
 
 set SSL=C:\OpenSSL-Win32\bin\openssl.exe
-set OPENSSL_CONF=C:\OpenSSL-Win32\bin\openssl.cfg
 
 if not exist %SSL% (
    echo OpenSSL isn't found in %SSL%
-   set SSL="%PROGRAMFILES%\OpenSSL\bin\openssl.exe"
-   set OPENSSL_CONF="%PROGRAMFILES%\OpenSSL\bin\openssl.cfg"
+   set SSL="%PROGRAMFILES%\OpenSSL-Win32\bin\openssl.exe"
+)
+
+if not exist %SSL% (
+   echo OpenSSL isn't found in %SSL%
+   set SSL="%PROGRAMFILES(X86)%\OpenSSL-Win32\bin\openssl.exe"
 )
 
 if not exist %SSL% (
@@ -33,13 +36,13 @@ echo This script must be run from the ADCH++ program folder...
 goto end 
 
 :found
-echo Proceeding with key generation
-call "%SSL%" genrsa -out privkey.pem 2048
+echo Proceeding with key generation...
+call %SSL% genrsa -out privkey.pem 2048
 cls
-call "%SSL%" dhparam -outform PEM -out dhparam.pem 2048
+call %SSL% dhparam -outform PEM -out dhparam.pem 2048
 cls
-call "%SSL%" req -new -x509 -key privkey.pem -out cacert.pem -days 1095
-cls
+call %SSL% req -new -x509 -key privkey.pem -out cacert.pem -days 1095
+
 mkdir certs
 cd certs
 mkdir trusted
